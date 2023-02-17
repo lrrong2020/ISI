b5116d0df90ab95d8844497aa9ff6bc9f24ec99a
Grant <2395642493@qq.com>
2023-02-17 11:47:59 +0800

    full import element-ui

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
