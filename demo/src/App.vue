329b2b5041e99b7e7ee05d8bf4cb80eb733b92dd
Grant <2395642493@qq.com>
2023-02-20 14:56:04 +0800

    test router

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,23 +1,26 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+   
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/brazil">Brazil</router-link> |
+        <router-link to="/hawaii">Hawaii</router-link> |
+        <router-link to="/panama">Panama</router-link> |
+        <router-link to="/jamaica">Jamaica</router-link>
       </nav>
+
     </div>
   </header>
 
-  <RouterView />
+  <router-view />
 </template>
 
 <style scoped>
--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Brazil from '../views/Brazil.vue'
+import Hawaii from '../views/Hawaii.vue'
+import Panama from '../views/Panama.vue'
+import Jamaica from '../views/Jamaica.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +21,27 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/brazil',
+      name: 'brazil',
+      component: Brazil
+    },
+    {
+      path: '/hawaii',
+      name: 'hawaii',
+      component: Hawaii
+    },
+    {
+      path: '/panama',
+      name: 'panama',
+      component: Panama
+    },
+    {
+      path: '/jamaica',
+      name: 'jamaica',
+      component: Jamaica
+    },
   ]
 })
 
