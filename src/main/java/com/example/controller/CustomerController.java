45661c9c9734b9b6b755c0249293ae7c9b3c07fd
Runrong Liu <lrrong2020@gmail.com>
2023-02-24 14:50:33 +0800

    CORS enable 5173

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -2,7 +2,9 @@ package com.example.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Customer;
 import com.example.service.CustomerService;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class CustomerController {
 	
