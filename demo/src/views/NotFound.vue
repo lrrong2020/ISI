d1a3a94c54dff77018bc02b62d293afd8c93c401
Grant <2395642493@qq.com>
2023-02-27 05:22:53 +0800

    delete

--- demo/src/views/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<div>
-    <van-empty image="error" description="404 Not Found" />
-    <p>
-        Oops! The page you're looking for doesn't exist. <router-link to="/">Go back home</router-link>
-    </p>
-</div>
-</template>
