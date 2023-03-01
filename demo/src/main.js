ff94c0a15808cc4ffbfb9f92085fd7b833084177
Grant <2395642493@qq.com>
2023-03-01 20:33:25 +0800

    config

--- demo/src/main.js
+++ demo/src/main.js
@@ -3,12 +3,22 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'lib-flexible/flexible'
+import 'vant/lib/index.css';
+import '@vant/touch-emulator';
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+//Vant
+import { Button } from 'vant';
+app.use(Button);
+
 app.use(createPinia())
+app.use(VueAxios, axios)
 app.use(router)
 
 app.mount('#app')
--- demo/src/views/HomeView.vue
+++ demo/src/views/HomeView.vue
@@ -4,6 +4,10 @@
 
 <template>
   <main>
-    <TheWelcome />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </main>
 </template>
