1e5fac6b93432aa7678a946315667475674ae8c4
Grant <2395642493@qq.com>
2023-03-01 20:54:12 +0800

    add style

--- demo/src/main.js
+++ demo/src/main.js
@@ -10,15 +10,59 @@ import 'vant/lib/index.css';
 import '@vant/touch-emulator';
 
 import './assets/main.css'
+import './common/style/theme.css'
+import 'vant/es/toast/style'
 
 const app = createApp(App)
 
 //Vant
-import { Button } from 'vant';
-app.use(Button);
+import { ActionBar, ActionBarIcon, ActionBarButton, Divider, Popup, Overlay, Loading, Dialog, ContactCard, Form, AddressEdit, AddressList, Field, CellGroup, Cell, SwipeCell, Icon, Stepper, Card, Checkbox, CheckboxGroup, Button, Swipe, SwipeItem, PullRefresh, List, Tab, Tabs, SubmitBar, Toast, Skeleton } from 'vant';
+app.use(ActionBarButton)
+  .use(ActionBarIcon)
+  .use(ActionBar)
+  .use(Divider)
+  .use(Popup)
+  .use(Overlay)
+  .use(Loading)
+  .use(Dialog)
+  .use(Toast)
+  .use(ContactCard)
+  .use(Form)
+  .use(AddressEdit)
+  .use(AddressList)
+  .use(Field)
+  .use(CellGroup)
+  .use(Cell)
+  .use(SwipeCell)
+  .use(Icon)
+  .use(Stepper)
+  .use(Card)
+  .use(Button)
+  .use(Swipe)
+  .use(SwipeItem)
+  .use(PullRefresh)
+  .use(List)
+  .use(Tab)
+  .use(Tabs)
+  .use(SubmitBar)
+  .use(Checkbox)
+  .use(CheckboxGroup)
+  .use(Skeleton)
 
 app.use(createPinia())
 app.use(VueAxios, axios)
 app.use(router)
+
+// 全局过滤器
+app.config.globalProperties.$filters = {
+    prefix(url) {
+      if (url && url.startsWith('http')) {
+        return url
+      } else {
+        url = `http://backend-api-01.newbee.ltd${url}`
+        return url
+      }
+    }
+  }
 
 app.mount('#app')
--- demo/src/views/HomeView.vue
+++ demo/src/views/HomeView.vue
@@ -9,5 +9,10 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div>
+      <p>
+        AAAAA
+      </p>
+    </div>
   </main>
 </template>
