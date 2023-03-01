61384fe9001e00af6d1cd6d16ae21a3dc776a25d
Grant <2395642493@qq.com>
2023-03-02 01:11:59 +0800

    detail page

--- demo/src/main.js
+++ demo/src/main.js
@@ -9,12 +9,16 @@ import './assets/reset.css'
 import '@vant/touch-emulator';
 
 
-//Vant global reference
-// import 'vant/lib/index.css';
-// import { Button,Icon } from 'vant';
-// app.use(Button).use(Icon);
-
 const app = createApp(App)
+
+//Vant global reference
+import 'vant/lib/index.css';
+import { Button,Icon,Swipe,SwipeItem,Search,ConfigProvider,Card,Tag,Pagination,CellGroup,Field,NavBar,Tabbar,TabbarItem,ActionBar,
+ActionBarIcon,ActionBarButton,Grid,GridItem,Tabs,Tab } from 'vant';
+
+app.use(Button).use(Icon).use(Swipe).use(SwipeItem).use(Search).use(ConfigProvider).use(Card).use(Tag)
+.use(Pagination).use(CellGroup).use(Field).use(NavBar).use(Tabbar).use(TabbarItem).use(ActionBar).use(ActionBarIcon)
+.use(ActionBarButton).use(Grid).use(GridItem).use(Tabs).use(Tab);
 
 app.use(router)
 app.use(store)
--- demo/src/views/detail/index.vue
+++ demo/src/views/detail/index.vue
@@ -1,45 +1,67 @@
 <script>
+import { ref } from 'vue';
+
 export default {
     name: "Detail",
     setup() {
-        const images = [
-            'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
-            'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
-        ];
-        return { images };
+        const active = ref(0);
+
+        const text = 'Vant is lightweight, customizable and customizable, 2017 at present. . . Official official official official vue 2 vue 2 vue 3 vue 3 vue 3 version version version version version WeChat WeChat applet';
+        return { text,active };
     },
 }
 </script>
 
 <template>
-    <div class="swipe">
-        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-            <van-swipe-item>1</van-swipe-item>
-            <van-swipe-item>2</van-swipe-item>
-            <van-swipe-item>3</van-swipe-item>
-            <van-swipe-item>4</van-swipe-item>
-        </van-swipe>
-    </div>
-    <div class="main">
-        <p>aaaaaaaaaaa</p>
+    <div class="bg">
+        <div class="swipe">
+            <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+                <van-swipe-item>1</van-swipe-item>
+                <van-swipe-item>2</van-swipe-item>
+                <van-swipe-item>3</van-swipe-item>
+                <van-swipe-item>4</van-swipe-item>
+            </van-swipe>
+        </div>
+        <div class="main">
+            <div class="price">
+                <p><van-tag plain type="danger">Price</van-tag> $1000</p>
+            </div>
+            <div class="name">
+                <p> Product Name</p>
+            </div>
+            <div class="desc">
+                <p><van-tag plain type="primary">Brand</van-tag>{{ text }}</p>
+            </div>
+        </div>
+        <div class="prop">
+            <van-tabs v-model:active="active" swipeable>
+            <van-tab v-for="index in 3" :title="'Prop' + index">
+                Prop {{ index }}
+            </van-tab>
+            </van-tabs>
+        </div>
     </div>
 </template>
 
-<style>
- .my-swipe .van-swipe-item {
+<style lang="less" scoped>
+.bg{
+    .my-swipe .van-swipe-item {
     color: #fff;
     font-size: 20px;
     line-height: 150px;
     text-align: center;
     background-color: #39a9ed;
   }
-  .image{
-    width: 100%;
-    height: 100%;
-  }
   .main{
-    margin: 20px 10px 0px 10px;
-    border-radius: 20px;
+    border-radius: 15px;
     background-color: aqua;
+    .name{
+        font-size: 20px;
+    }
+    .desc{
+        font-size: 15px;
+    }
   }
+}
+
 </style>
