384518fe095c3bc44252866a4dd5ba3a4df84ba1
Runrong Liu <lrrong2020@gmail.com>
2023-03-15 00:51:11 +0800

    Replace Lables according to requirements:

--- demo/src/views/Vendor/add/addProduct.vue
+++ demo/src/views/Vendor/add/addProduct.vue
@@ -1,6 +1,9 @@
 <template>
 <van-cell-group inset>
-  <van-field v-model="value" label="Label" placeholder="Text" />
+  <van-field v-model="productName" ref="productName" label="productName" placeholder="The name of the product" />
+  <van-field v-model="brand" ref="brand" label="brand" placeholder="Which brand?" />
+  <van-field v-model="price" ref="price" label="price" placeholder="Set price to 2dp" />
+  <van-field v-model="image" ref="image" label="image" placeholder="imageeeeee" />
 </van-cell-group>
 </template>
 
@@ -8,8 +11,17 @@
 
 export default {
   setup() {
-    const value = ref('');
-    return { value };
+    const productName = ref('productName');
+    const brand = ref('brand');
+    const price = ref('price');
+    const image = ref('image');
+
+    return { 
+      productName,
+      brand,
+      price,
+      image
+    };
   },
 };
 </script>
