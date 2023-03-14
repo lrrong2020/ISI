f0f1047a632ece02a84c527c0a45e2201a3f9441
Runrong Liu <lrrong2020@gmail.com>
2023-03-15 02:08:08 +0800

    Get Submit event in console

--- demo/src/views/Vendor/add/addProduct.vue
+++ demo/src/views/Vendor/add/addProduct.vue
@@ -1,26 +1,99 @@
 <template>
-<van-cell-group inset>
-  <van-field v-model="productName" ref="productName" label="productName" placeholder="The name of the product" />
-  <van-field v-model="brand" ref="brand" label="brand" placeholder="Which brand?" />
-  <van-field v-model="price" ref="price" label="price" placeholder="Set price to 2dp" />
-  <van-field v-model="image" ref="image" label="image" placeholder="imageeeeee" />
-</van-cell-group>
+  <van-form @submit="onSubmit">
+    <van-cell-group inset>
+      <van-field
+        v-model="productName"
+        name="Product"
+        label="Product"
+        placeholder="The name of the product"
+        :rules="[{ required: true, message: 'Product Name is required' }]"
+      />
+
+      <van-field 
+      v-model="brand" 
+      name="Brnad"
+      label="Brand" 
+      placeholder="Which brand?" 
+      :rules="[{ required: true, message: 'Brand is required' }]"
+      />
+
+      <van-field
+        v-model="price"
+        name="Price"
+        type="number"
+        label="Price"
+        placeholder="Set price to 2dp"
+        :rules="[{ required: true, message: 'Price is required' }]"
+      />
+
+      <van-field 
+      v-model="image" 
+      name="Image"
+      type="url" 
+      label="Image" 
+      placeholder="imageeeeee" 
+      :rules="[{ required: true, message: 'Image is required' }]"
+      />
+    </van-cell-group>
+
+    <div style="margin: 16px">
+      <van-button round block type="primary" native-type="submit"> Submit </van-button>
+    </div>
+  </van-form>
+
+
+  <van-form @submit="onSubmit">
+  <van-cell-group inset>
+    <van-field
+      v-model="username"
+      name="Username"
+      label="Username"
+      placeholder="Username"
+      :rules="[{ required: true, message: 'Username is required' }]"
+    />
+    <van-field
+      v-model="password"
+      type="password"
+      name="Password"
+      label="Password"
+      placeholder="Password"
+      :rules="[{ required: true, message: 'Password is required' }]"
+    />
+  </van-cell-group>
+  <div style="margin: 16px;">
+    <van-button round block type="primary" native-type="submit">
+      Submit
+    </van-button>
+  </div>
+</van-form>
+
 </template>
 
-<script>import { ref } from 'vue';
+<script>
+import { ref } from "vue";
 
 export default {
   setup() {
-    const productName = ref('productName');
-    const brand = ref('brand');
-    const price = ref('price');
-    const image = ref('image');
+    const productName = ref("");
+    const brand = ref("");
+    const price = ref("");
+    const image = ref("");
 
-    return { 
+    const username = ref('');
+    const password = ref('');
+
+    const onSubmit = (values) => {
+      console.log("submit", values);
+    };
+
+    return {
       productName,
       brand,
       price,
-      image
+      image,
+      onSubmit,
+      username,
+      password,
     };
   },
 };
